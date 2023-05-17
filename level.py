54f9038585d20726dc10e00f8ca7e0384264856e
Janahan Siva <57200038+ZeDUCC@users.noreply.github.com>
2023-05-17 12:01:30 -0400

    added sideways scrolling functionality

--- level.py
+++ level.py
@@ -27,6 +27,20 @@ class Level:
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width/4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width*3/4 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
     def run(self):
 
         self.tiles.update(self.world_shift) #level tiles
@@ -34,3 +48,4 @@ class Level:
 
         self.player.update() #player character
         self.player.draw(self.display_surface) 
+        self.scroll_x()
