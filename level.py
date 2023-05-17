94c192a1e7c52305b2c683f0bf5d462ad9e78af7
Janahan Siva <57200038+ZeDUCC@users.noreply.github.com>
2023-05-17 11:25:29 -0400

    added level file

---
+++ level.py
@@ -0,0 +1,17 @@
+import pygame
+from tiles import Tile
+
+class Level:
+    def __init__(self, level_data, surface):
+        self.display_surface = surface
+        self.setup_level(level_data)
+
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                print(f'{row_index},{col_index}:{cell}')
+
+    def run(self):
+        self.tiles.draw(self.display_surface)
